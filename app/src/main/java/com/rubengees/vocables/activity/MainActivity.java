c80cb994a3588d5c49cf6eab8dc8498a634e9a48
Ruben Gees <geesruben@yahoo.de>
2015-10-17 12:18:29 +0200

    Update libs; Fix ads

--- app/src/main/java/com/rubengees/vocables/activity/MainActivity.java
+++ app/src/main/java/com/rubengees/vocables/activity/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -381,8 +382,8 @@ public class MainActivity extends ExtendedToolbarActivity implements EvaluationD
      * @param title    The Title of the Fragment
      */
     public void setFragment(@NonNull Fragment fragment, @Nullable String title) {
-        int color = getResources().getColor(R.color.primary);
-        int darkColor = getResources().getColor(R.color.primary_dark);
+        int color = ContextCompat.getColor(this, R.color.primary);
+        int darkColor = ContextCompat.getColor(this, R.color.primary_dark);
 
         setFragment(fragment, title, color, darkColor);
     }
